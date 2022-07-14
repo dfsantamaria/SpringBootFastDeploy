45c182c623c08756b0cc564304798b21e4894604
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-07-14 21:35:35 +0200

    registering -- started

--- src/main/java/it/unict/spring/application/controller/access/LoginController.java
+++ src/main/java/it/unict/spring/application/controller/access/LoginController.java
@@ -4,10 +4,16 @@
  */
 package it.unict.spring.application.controller.access;
 
+import it.unict.spring.application.persistence.model.user.UserAccount;
+import it.unict.spring.application.service.user.UserService;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -18,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/public/api/access")
 public class LoginController
 { 
+    @Autowired
+    UserService userService;
+    
      @RequestMapping("signin")
      public String viewlogin(HttpServletRequest request, Model model)
      {              
@@ -29,5 +38,19 @@ public class LoginController
      {              
         return "public/access/signin";
      }  
+     
+     @RequestMapping("register")
+     public String viewRegister(HttpServletRequest request, Model model, UserAccount user)
+     {         
+         model.addAttribute("user", user);       
+         return "public/access/register";
+     }
+     
+     @RequestMapping(value="registerUser", method = RequestMethod.POST)
+     public String registerUser(@ModelAttribute("user") UserAccount user, Model model)
+     {         
+         userService.save(user);
+         return "public/home";
+     }
 }
 
