b3e754724bd3c5a7ba3203848917308c167249b4
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-09-12 16:37:58 +0200

    privacy policy added to every page

--- src/main/resources/static/public/js/cookie.js
+++ src/main/resources/static/public/js/cookie.js
@@ -3,12 +3,13 @@ $(document).ready(function()
     // Check if the user already accepted 
     if (window.localStorage.getItem('accept_cookies'))
     {
-        $('#cookie-consent').css('visibility','hidden');
+        $('#cookie-consent').css('display','none');
     }
 
     $("#cookie-ok-button").click(function(){
         // Save on LocalStorage
         window.localStorage.setItem('accept_cookies', true);
+        $('#cookie-consent').css('display','none');
         $('#cookie-consent').fadeOut();
     }); 
 });
