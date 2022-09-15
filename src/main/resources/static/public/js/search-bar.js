06b44cef4656957f42b57adc3fcdb5be2b043f46
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-09-15 19:43:15 +0200

    minor corrections to usersView

--- src/main/resources/static/public/js/search-bar.js
+++ src/main/resources/static/public/js/search-bar.js
@@ -2,6 +2,6 @@ $(document).ready(function()
  {
    $('.select2-multiple-choices').select2(
     {          
-     placeholder: 'Select one or more fields'
+     placeholder: 'Select one or more fields'     
     });    
  });    
