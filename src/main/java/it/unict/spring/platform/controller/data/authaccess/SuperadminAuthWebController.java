44e72fe898eb4f34295eef0705c3080f6e9bc78f
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-09-19 16:34:12 +0200

    minor corrections to usersView

--- src/main/java/it/unict/spring/platform/controller/data/authaccess/SuperadminAuthWebController.java
+++ src/main/java/it/unict/spring/platform/controller/data/authaccess/SuperadminAuthWebController.java
@@ -7,9 +7,7 @@ package it.unict.spring.platform.controller.data.authaccess;
  * 
  */
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class SuperadminAuthWebController
 {
         
-    @GetMapping("/hello")
+    /*@GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name)
     {
         return String.format("Hello %s!", name);
-    }
+    }*/
 }
