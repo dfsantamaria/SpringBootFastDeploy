3ca1fd13904d19d00aa13095e376940b179512d4
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-10-27 11:44:10 +0200

    404 custom error management

---
+++ src/main/java/it/unict/spring/platform/controller/access/CustomErrorController.java
@@ -0,0 +1,24 @@
+package it.unict.spring.platform.controller.access;
+
+/**
+ *
+ * @author Daniele Francesco Santamaria daniele.santamaria@unict.it
+ * -- https://github.com/dfsantamaria/SpringBootFastDeploy.git --
+ * 
+ */
+
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/public/access")
+public class CustomErrorController implements ErrorController
+{    
+    @RequestMapping("/error")
+    public ModelAndView manageError() {
+        return new ModelAndView("redirect:/public/api/access/error/forbidden");
+    }
+}
