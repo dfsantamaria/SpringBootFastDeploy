c65dab0bdef2f467031bc522db0a7837f06b8b0e
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2022-11-05 18:02:08 +0100

    tooltip

---
+++ src/main/resources/static/public/js/main/tooltip.js
@@ -0,0 +1,6 @@
+ /* @author Daniele Francesco Santamaria daniele.santamaria@unict.it 
+ -- https://github.com/dfsantamaria/SpringBootFastDeploy.git -- */
+ 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
