396b6cfc5034aec2274bc4b71424e3ae87a933dd
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2023-05-24 19:17:52 +0200

    virtuoso integration

--- src/main/java/it/unict/spring/platform/controller/access/SigninController.java
+++ src/main/java/it/unict/spring/platform/controller/access/SigninController.java
@@ -7,23 +7,14 @@ package it.unict.spring.platform.controller.access;
  * 
  */
 
-import it.unict.spring.platform.configuration.security.login.CustomLoginSuccessHandler;
 import it.unict.spring.platform.dto.user.LoginDTO;
 import it.unict.spring.platform.service.platform.PlatformStatusService;
-import it.unict.spring.platform.service.security.CustomUserDetailsService;
-import it.unict.spring.platform.utility.user.CustomUserDetails;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
