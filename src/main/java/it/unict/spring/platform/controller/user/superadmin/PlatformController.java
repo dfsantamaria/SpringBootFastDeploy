840a5ec236538681b7366f98eb998708e1ce3e1e
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2023-05-25 08:27:31 +0200

    rest-full signout

--- src/main/java/it/unict/spring/platform/controller/user/superadmin/PlatformController.java
+++ src/main/java/it/unict/spring/platform/controller/user/superadmin/PlatformController.java
@@ -7,44 +7,29 @@ package it.unict.spring.platform.controller.user.superadmin;
  * 
  */
 
-import it.unict.spring.platform.persistence.model.user.Privilege;
 import it.unict.spring.platform.service.platform.PlatformStatusService;
-import it.unict.spring.platform.service.user.UserService;
-import it.unict.spring.platform.utility.user.CustomUserDetails;
-import it.unict.spring.platform.utility.user.ModelTemplate;
-import java.util.Locale;
-import java.util.Set;
+import org.json.HTTP;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/auth/api/superadmin/platform")
 public class PlatformController
-{
-    @Autowired
-    UserService userService;
+{    
     @Autowired 
-    PlatformStatusService platformService;
-    
-    @RequestMapping(value = "/viewPlatformManagement", method = RequestMethod.GET)
-    public ModelAndView platformManagView(Locale locale, @AuthenticationPrincipal CustomUserDetails user, Model model)
-    {
-      Set<Privilege> setPriv = userService.findPrivilegeFromCustomUserDetails(user);  
-      ModelTemplate.setNavBar(setPriv.iterator(), model);       
-      model.addAttribute("maintenance", platformService.isMaintenanceMode());
-      return new ModelAndView("auth/superadmin/platform/PlatformManagement"); 
-    }
-    
-     @RequestMapping(value = "/toggleMaintenance", method = RequestMethod.GET)
-     public ModelAndView platformManagView(Model model)
+    PlatformStatusService platformService;    
+        
+    @GetMapping("toggleMaintenance")
+    public ResponseEntity<String> toggleMaintenanceMode()
      {
-        platformService.toggleMaintenanceMode();
-        return new ModelAndView("redirect:/auth/api/superadmin/platform/viewPlatformManagement");
+        JSONObject obj=new JSONObject();
+        obj.put("status", platformService.toggleMaintenanceMode());
+        return new ResponseEntity<>(obj.toString(), HttpStatus.OK);       
      }
 }
